d9136d352ac79e5783d41e1ea055e150822598cb
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-21 00:30:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import useWebAnimations,{headShake} from "@wellyshen/use-web-animations";
+import Queen_and_alice from './components/queen_and_alice'
+import Background1 from './components/background1';
+import Background2 from './components/background2';
+import Forground1 from './components/Forground1';
+import Forground2 from './components/Forground2';
+
+
 
 function App() {
+
   return (
     <div>
-        Hello World
+        <div class="sky"></div>
+        <div class="earth">
+            <Queen_and_alice />
+        </div>
+        <Background1 />
+        <Background2 />
+        <Forground1 />
+        <Forground2 />
+
     </div>
-  );
+    
+    );
 }
 
 export default App;
